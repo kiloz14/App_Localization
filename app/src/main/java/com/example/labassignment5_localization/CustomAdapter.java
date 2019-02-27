04115ee39a2a57299db86c0f638833a1e62b7845
kiloz14 <tug82420@temple.edu>
2019-02-26 22:56:27 -0500

    Set up to start Lab 5

---
+++ app/src/main/java/com/example/labassignment5_localization/CustomAdapter.java
@@ -0,0 +1,46 @@
+package com.example.labassignment5_localization;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+public class CustomAdapter extends BaseAdapter{
+
+    private Context context;
+    String color[] = new String[]{"blue","magenta","red","yellow", "green", "black","darkgray","cyan","lightgray", "gray" };
+
+
+    public CustomAdapter(Context context){
+        this.context = context;
+    }
+
+    @Override
+    public int getCount(){
+        return color.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return color[position];
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final TextView textView = new TextView(context);
+        textView.findViewById(R.id.spinner);
+        String colorName = (String) getItem(position);
+        textView.setText(colorName.toUpperCase());
+        textView.setBackgroundColor(Color.parseColor(color[position]));
+
+        return textView;
+    }
+
+}
--- app/src/main/java/com/example/labassignment5_localization/MainActivity.java
+++ app/src/main/java/com/example/labassignment5_localization/MainActivity.java
@@ -1,13 +1,59 @@
 package com.example.labassignment5_localization;
 
+import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final  String HOLDER = "";
+    boolean well = false ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        final Spinner spinner = findViewById(R.id.spinner);
+        final ConstraintLayout layout = findViewById(R.id.layout);
+
+        getSupportActionBar().setTitle("PaletteActivity");
+
+        spinner.setAdapter(new CustomAdapter(this)) ;
+
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent intent = new Intent(MainActivity.this, CanvasActivity.class);
+                String colSelected = spinner.getSelectedItem().toString();
+
+                if(well){
+
+                    intent.putExtra("HOLDER" , colSelected);
+                    startActivity(intent);
+                }
+
+                else
+                {
+                    well = true;
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
     }
 }
